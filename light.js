b68ef9a7d980651035d0f73d62dbb5ee62fa0fc5
Alexandre ROC <alexandre.roc_ssii@i-bp.fr>
2015-06-16 16:28:47 +0200

    put documents

--- light.js
+++ light.js
@@ -1,30 +1,117 @@
 var PouchDB = require('pouchdb');
+var ldown = require('sqldown');
+var fs = require('fs');
+var Promise = require("bluebird");
+var fsAsync = Promise.promisifyAll(fs);
+var db;
 
-var db_old = new PouchDB('./datas/alex_db.json', {
-  db: require('jsondown')
+var db_old = new PouchDB('./datas/alex_db', {
+  db: ldown
 });
 
 db_old
   .destroy()
+
+.then(function () {
+  console.log('la base alex_db est détruite');
+})
 
 .catch(function (error) {
   console.log('delete db ', error);
 })
 
 .then(function () {
-  console.log('la base alex_db est détruite');
-  var db = new PouchDB('./datas/alex_db.json', {
-    db: require('jsondown')
-  });
-  var attachment = new Buffer(['Is there life on Mars?']);
 
-  return db.putAttachment('doc', 'att.txt', attachment, 'text/plain');
+  return fsAsync.readFileAsync('./picture.jpg');
+
+})
+
+.then(function (data) {
+  db = new PouchDB('./datas/alex_db', {
+    db: ldown
+  });
+
+  //console.log("datas : ", data);
+
+  var doc1 = {
+    "_id": "docb",
+    "date": Date.now(),
+    "from": "Alexandre",
+    "_attachments": {
+      "picture2.jpg": {
+        "content_type": "image/jpeg",
+        "data": data
+      }
+    }
+  };
+
+  return db.put(doc1);
+
+})
+
+.then(function () {
+  return db.get('docb');
+
 })
 
 .then(function (result) {
   console.log(result);
 })
 
-.catch(function (err) {
-  console.log(err);
+.then(function () {
+
+  return fsAsync.readFileAsync('./picture.jpg');
+
+})
+
+.then(function (data) {
+  var doc2 = {
+    "_id": "doca",
+    "date": Date.now(),
+    "from": "Alexandre",
+    "_attachments": {
+      "picture1.jpg": {
+        "content_type": "image/jpeg",
+        "data": data
+      }
+    }
+  };
+
+  return db.put(doc2);
+
+})
+
+.then(function () {
+  return db.get('doca');
+
+})
+
+.then(function (result) {
+  console.log(result);
+})
+
+.then(function () {
+  /*return db.allDocs({
+    descending: true
+  });*/
+
+  return db.query(
+    function (doc, emit) {
+      emit([doc.date, doc._id]);
+    }, {
+      descending: true,
+      include_docs: true
+    });
+})
+
+.then(function (all) {
+    console.log(all);
+  })
+  .catch(function (err) {
+    console.log(err);
+  })
+
+.then(function () {
+  console.log("terminé");
+  process.abort();
 });
